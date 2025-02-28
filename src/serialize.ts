fba9430a1ebb9f483175ada24f6bc36d26ab027c
Matveyx25 <matveyx25@gmail.com>
2025-02-28 14:03:23 +0400

    Fix br tags

--- src/serialize.ts
+++ src/serialize.ts
@@ -13,7 +13,7 @@ const isLeafNode = (node: BlockType | LeafType): node is LeafType => {
 
 const VOID_ELEMENTS: Array<keyof NodeTypes> = ['thematic_break', 'image'];
 
-const BREAK_TAG = '<br>';
+const BREAK_TAG = '';
 
 export default function serialize(
   chunk: BlockType | LeafType,
